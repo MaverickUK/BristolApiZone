4c3e1f3d88badb207e62f77a836e0e0347cd5b92
Luke Parry <lparry@zonedigital.com>
2016-10-08 14:24:12 +0100

    Add schedule controller

---
+++ BristolApiZone/Controllers/ScheduleController.cs
@@ -0,0 +1,25 @@
+﻿using BristolApiZone.Data.Context;
+using BristolApiZone.Domain.Models;
+using System.Web.Mvc;
+
+namespace BristolApiZone.Web.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Schedule schedule)
+        {
+            using (var context = new DatabaseContext())
+            {
+
+            }
+
+            return View(schedule);
+        }
+    }
+}
