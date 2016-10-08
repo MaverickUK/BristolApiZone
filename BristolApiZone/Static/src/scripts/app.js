35625dce68b1d439a76dba35d6dccba3411e7e93
Daniel Williams <dwilliams@thisiszone.com>
2016-10-08 14:20:25 +0100

    Add form stuff

--- BristolApiZone/Static/src/scripts/app.js
+++ BristolApiZone/Static/src/scripts/app.js
@@ -1,13 +1,13 @@
 import $ from 'jquery';
 
-import Map from './modules/map';
+import Autocomplete from './modules/autocomplete';
 
 function init() {
 
-    let mapContext = $('[data-behavior~="map"]');
-    if (mapContext.length > 0) {
-        for (var map of mapContext) {
-            new Map(map);
+    let autocompleteContext = $('[data-behavior~="autocomplete"]');
+    if (autocompleteContext.length > 0) {
+        for (var autocomplete of autocompleteContext) {
+            new Autocomplete(autocomplete);
         }
     }
 }
